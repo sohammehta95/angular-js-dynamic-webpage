97c14a52c66edde48d7830313f1136fe9c207c07
sohammehta95 <32079958+sohammehta95@users.noreply.github.com>
2018-02-26 15:00:20 -0500

    3.1

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ description: 'This is cool',
 canPurchase: true,
 soldOut: true ,
 images: [ 
- {
- full: 'd1f.png',
-thumb: 'd1t.jpg'  }, 
-{
-full: "d2f.jpg",
-thumb: 'd2t.jpg' } ],
+{ full: 'd1f.png', thumb: 'd1t.jpg' }, 
+{ full: "d2f.jpg", thumb: 'd2t.jpg' } ],
+reviews: [ 
+{ stars: 5, body: "I love this product!",  author: "joe@thomas.com" },  
+{ stars: 1, body: "This product sucks",  author: "tim@hater.com" } ]
  },
  { 
 name: "Pentagonal Gem", 
