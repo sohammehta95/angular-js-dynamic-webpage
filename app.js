7e4a26cba50e111189d53aa43eb4fa1073e7e84e
sohammehta95 <32079958+sohammehta95@users.noreply.github.com>
2018-02-25 22:49:38 -0500

    1.5

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 var app = angular.module('store', [ ]);
 
 app.controller('StoreController', function(){  
-	this.product = gem;
+	this.products = gems;
 });
 
-var gem = { 
+var gems = [
+{ 
 name: 'Dodecahedron', 
 price: 2.95, 
-description: 'This is cool'
- }
+description: 'This is cool',
+canPurchase: true,
+soldOut: true
+ },
+ { 
+name: "Pentagonal Gem", 
+price: 5.95, 
+description: "This is Super Cool", 
+canPurchase: true
+}
+]
   
 })();
